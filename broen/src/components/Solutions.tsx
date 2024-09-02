a47b80ccee72e575e7d912259cd531444589dbc6
cloak <frederikwer@gmail.com>
2024-09-02 18:58:55 +0100

    init

--- broen/src/components/Solutions.tsx
+++ broen/src/components/Solutions.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import '../index.css';
 
-import left from '../assets/Screenshot 2024-08-31 204632.png';
-import right from '../assets/Screenshot 2024-08-31 204748.png';
+import left from '../assets/0cc010ac07122ed66c40624c6c16258b.jpg';
+import right from '../assets/0cc010ac07122ed66c40624c6c16258b.jpg';
 
 const Solutions: React.FC = () => {
   return (
